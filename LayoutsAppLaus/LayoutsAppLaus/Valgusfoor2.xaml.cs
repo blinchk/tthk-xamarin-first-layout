74207de4352427a5ebadff6d04bf59b53206b062
Nikolas Laus <bchik3@gmail.com>
2020-09-09 15:03:45 +0300

    GitHub opens when headerClick, functions work, optimize UI.

--- LayoutsAppLaus/LayoutsAppLaus/Valgusfoor2.xaml.cs
+++ LayoutsAppLaus/LayoutsAppLaus/Valgusfoor2.xaml.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 
 using Xamarin.Forms;
+using Xamarin.Essentials;
 using Xamarin.Forms.PlatformConfiguration.TizenSpecific;
 using Xamarin.Forms.Xaml;
 using Label = Xamarin.Forms.Label;
@@ -14,11 +15,15 @@ namespace LayoutsAppLaus
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class Valgusfoor2 : ContentPage
     {
+        Frame punane, kollane, roheline, headerFrame;
+        Label punaneText, kollaneText, rohelineText, headerName;
+        Button onButton, offButton;
+        Uri uri;
         internal bool isEnableVf;
         public Valgusfoor2()
         {
             //   InitializeComponent();
-            Label headerName = new Label()
+            headerName = new Label()
             {
                 Text = "Valgusfoor",
                 HorizontalTextAlignment = TextAlignment.Start,
@@ -27,15 +32,37 @@ namespace LayoutsAppLaus
                 FontAttributes = FontAttributes.Bold
             };
 
-            Frame headerFrame = new Frame()
+            headerFrame = new Frame()
             {
                 BackgroundColor = Color.FromHex("#2196F3"),
                 Padding = 10,
                 Content = headerName
             };
 
-            Frame punane = new Frame()
+            punaneText = new Label()
             {
+                Text = "Punane",
+                HorizontalOptions = LayoutOptions.Center,
+                VerticalOptions = LayoutOptions.Center
+            };
+
+            kollaneText = new Label()
+            {
+                Text = "Kollane",
+                HorizontalOptions = LayoutOptions.Center,
+                VerticalOptions = LayoutOptions.Center
+            };
+
+            rohelineText = new Label()
+            {
+                Text = "Roheline",
+                HorizontalOptions = LayoutOptions.Center,
+                VerticalOptions = LayoutOptions.Center
+            };
+
+            punane = new Frame()
+            {
+                Content = punaneText,
                 CornerRadius = 120,
                 BackgroundColor = Color.Gray,
                 VerticalOptions = LayoutOptions.FillAndExpand,
@@ -43,8 +70,9 @@ namespace LayoutsAppLaus
                 Margin = new Thickness(80, 7.5, 80, 0)
             };
 
-            Frame kollane = new Frame()
+            kollane = new Frame()
             {
+                Content = kollaneText,
                 CornerRadius = 120,
                 BackgroundColor = Color.Gray,
                 VerticalOptions = LayoutOptions.FillAndExpand,
@@ -52,8 +80,9 @@ namespace LayoutsAppLaus
                 Margin = new Thickness(80, 7.5, 80, 0)
             };
 
-            Frame roheline = new Frame()
+            roheline = new Frame()
             {
+                Content = rohelineText,
                 CornerRadius = 120,
                 BackgroundColor = Color.Gray,
                 VerticalOptions = LayoutOptions.FillAndExpand,
@@ -61,16 +90,16 @@ namespace LayoutsAppLaus
                 Margin = new Thickness(80, 0, 80, 7.5)
             };
 
-            Button onButton = new Button()
+            onButton = new Button()
             {
-                Text = "ВКЛ",
+                Text = "SISSE",
                 HorizontalOptions = LayoutOptions.FillAndExpand,
                 VerticalOptions = LayoutOptions.FillAndExpand,
             };
 
-            Button offButton = new Button()
+            offButton = new Button()
             {
-                Text = "ВКЛ",
+                Text = "VÄLJA",
                 HorizontalOptions = LayoutOptions.FillAndExpand,
                 VerticalOptions = LayoutOptions.FillAndExpand,
             };
@@ -86,12 +115,20 @@ namespace LayoutsAppLaus
             {
                 Children = { headerFrame, punane, kollane, roheline, buttonsLayout }
             };
+            onButton.Clicked += OnButton_Clicked;
+            offButton.Clicked += OffButton_Clicked;
 
             Content = commonLayout;
 
-            onButton.Clicked += OnButton_Clicked;
-            offButton.Clicked += OffButton_Clicked;
+            var tap = new TapGestureRecognizer();
+            tap.Tapped += Tap_Tapped;
+            punane.GestureRecognizers.Add(tap);
+            kollane.GestureRecognizers.Add(tap);
+            roheline.GestureRecognizers.Add(tap);
+            headerName.GestureRecognizers.Add(tap);
+            headerFrame.GestureRecognizers.Add(tap);
         }
+
         private void setNone()
         {
             punane.BackgroundColor = Color.Gray;
@@ -117,11 +154,40 @@ namespace LayoutsAppLaus
             roheline.BackgroundColor = Color.Green;
         }
 
+        private void Tap_Tapped(object sender, EventArgs e)
+        {
+            if (isEnableVf == false)
+            {
+                Frame fr = sender as Frame;
+                Label lbl = sender as Label;
+                if (fr == headerFrame || lbl == headerName)
+                {
+                    uri = new Uri("https://github.com/blinchk");
+                    OpenBrowser(uri);
+                }
+                else
+                    DisplayAlert("Lihtsalt valgusfoor", "Lülitage sisse valgusfoorit.", "Ikka");
+            }
+            else
+            {
+                Frame fr = sender as Frame;
+                if (fr == punane)
+                    punaneText.Text = "Seisa ja oota!";
+                else if (fr == kollane)
+                    kollaneText.Text = "Tähelepanu!";
+                else if (fr == roheline)
+                    rohelineText.Text = "Mine";
+            }
+        }
+        public async void OpenBrowser(Uri uri) => await Browser.OpenAsync(uri, BrowserLaunchMode.SystemPreferred);
+
         private void OffButton_Clicked(object sender, EventArgs e)
         {
             isEnableVf = false;
+            punaneText.Text = "Punane";
+            kollaneText.Text = "Kollane";
+            rohelineText.Text = "Roheline";
             setNone();
-            throw new NotImplementedException();
         }
 
         private void OnButton_Clicked(object sender, EventArgs e)
@@ -144,7 +210,6 @@ namespace LayoutsAppLaus
                     setNone();
                     break;
             }
-            throw new NotImplementedException();
         }
     }
 }
